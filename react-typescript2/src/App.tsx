55050345f0c9d2f4ab63237727685ecba74b6072
HS H <3262398@naver.com>
2023-06-21 11:57:05 +0900

    0621 리액트 타입스크립트

---
+++ react-typescript2/src/App.tsx
@@ -0,0 +1,20 @@
+import React from 'react';
+import logo from './logo.svg';
+import './App.css';
+import NoProps from './components/NoProps';
+import TypeClassComp from './components/TypeClassComp';
+import TypeFuncComp from './components/TypeFuncComp';
+
+function App() {
+  return (
+    <div className = "App">
+      <NoProps />
+      {/* 클래스형에서 props을 interface로 지정하면 호출할때도
+        * 자동완성으로 쓸 수 있다 */}
+      <TypeClassComp startNumber = {3} name = '자두'/>
+      <TypeFuncComp startNumber = {0} />
+    </div>
+  );
+}
+
+export default App;
