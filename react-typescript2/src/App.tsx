3bf2432b47255f201801b1fcf3194e00c095c4af
HS H <3262398@naver.com>
2023-06-23 16:19:15 +0900

    0623 FetchAPI

--- react-typescript2/src/App.tsx
+++ react-typescript2/src/App.tsx
@@ -4,15 +4,20 @@ import './App.css';
 import NoProps from './components/NoProps';
 import TypeClassComp from './components/TypeClassComp';
 import TypeFuncComp from './components/TypeFuncComp';
+import TypeArrowFuncComp from './components/TypeArrowFuncComp';
+import FetchComp from './components/FetchComp';
 
 function App() {
   return (
-    <div className = "App">
+    <div className="App">
       <NoProps />
-      {/* 클래스형에서 props을 interface로 지정하면 호출할때도
-        * 자동완성으로 쓸 수 있다 */}
-      <TypeClassComp startNumber = {3} name = '자두'/>
-      <TypeFuncComp startNumber = {0} />
+      {/** 클래스형에서 props을 interface로 지정하면 호출할때도
+       * 자동완성으로 쓸 수 있다 */}
+      <TypeClassComp startNumber={3} name='홍길동' check/>
+      <TypeFuncComp startNumber={0} />
+      <TypeArrowFuncComp startNumber={5}/>
+      <hr />
+      <FetchComp />
     </div>
   );
 }
